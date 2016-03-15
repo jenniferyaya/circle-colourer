de7562c2b4933d0b89241cfad41b629fb3f46c92
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-03-15 17:17:45 -0400

    Colour picker created

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var $body = $('body');
+var $form = $('form');
+var $input = $('#color-selector');
+var $color = $('.color');
+
+$input.on('change', function () {
+	var newColor = #ball;
+	var $ball.css('background-color', val());
+});
+
+
+
+//$('html').on('keydown', function () {
+//  var $div = $('<div>');
+
+//  $body.append($div);
+//  $div.addClass('ball');
+//  $div.css();
+//});
